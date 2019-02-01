bc8472df4397caa59c9493810e2a263a8f4de31d
dommrqz <jdominicm@hotmail.com>
2019-02-01 17:04:43 -0600

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,18 @@
+import unittest
+
+def multiply(x, y):
+    return x*y
+ 
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def test_numbers_3_4(self):
+        self.assertEqual( multiply(3,4), 12)
+ 
+    def test_strings_a_3(self):
+        self.assertEqual( multiply('a',3), 'aaa')
+ 
+if _name_ == '_main_':
+    unittest.main()
